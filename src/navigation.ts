c75c0cfb33a2ac5667ca06c93104e4eb94e5628a
kacan98 <kacan@dynaway.com>
2025-01-30 01:03:36 +0100

    Revert header

--- src/navigation.ts
+++ src/navigation.ts
@@ -134,11 +134,12 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/detonormalanormala/' },
+    {
+      ariaLabel: 'Spotify',
+      icon: 'tabler:brand-spotify',
+      href: 'https://open.spotify.com/show/4vLmISJW3dYqLc66bwXbTM',
+    },
   ],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://www.linkedin.com/in/kcancara/" target="_blank"> Karel Čančara</a> · All rights reserved.
